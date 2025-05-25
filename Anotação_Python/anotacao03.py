2b5a50de429adab97046e0abd7a058efc88c65fa
Greison Montenari <165381030+GreisonMontenari@users.noreply.github.com>
2025-05-25 09:24:56 -0300

    Rename anotacao03.py to Anotação_Python/anotacao03.py

---
+++ Anotação_Python/anotacao03.py
@@ -0,0 +1,7 @@
+""" Método format """
+
+faturamento = 2000
+custo = 200
+lucro = faturamento - custo
+
+print('O faturamento da loja foi {}. O custo da loja foi {}. Assim, o lucro da loja foi {}'.format(faturamento, custo, lucro))
--- anotacao03.py
+++
@@ -1,7 +0,0 @@
-""" Método format """
-
-faturamento = 2000
-custo = 200
-lucro = faturamento - custo
-
-print('O faturamento da loja foi {}. O custo da loja foi {}. Assim, o lucro da loja foi {}'.format(faturamento, custo, lucro))
